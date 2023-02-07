3f4358950cae6d969162a1f40af9ea322c6cc4f4
carlosSimplicio <carlos.edsmoura@gmail.com>
2023-02-06 22:22:57 -0300

    add sample images/experimenting with serverprops

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -2,7 +2,35 @@ import React from "react";
 import styles from "../styles/feed.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 
-const Feed = () => {
+interface FeedProps {
+  imagePaths: Array<string>;
+}
+
+const getFeedImages = async () => {
+  var fs = require("node:fs/promises");
+  let imagePaths: Array<string> = [];
+  try {
+    const files: Array<string> = await fs.readdir(
+      "/home/carlossimplicio/Documents/instagrao/public/feed-images"
+    );
+    imagePaths = files.map((file) => "feed-images/" + file);
+  } catch (error) {
+    console.log(error);
+  }
+  return imagePaths;
+};
+
+export const getServerSideProps = async () => {
+  const imagePaths = await getFeedImages();
+  return {
+    props: {
+      imagePaths,
+    },
+  };
+};
+
+const Feed: React.FC<FeedProps> = ({ imagePaths }) => {
+  console.log(imagePaths);
   return (
     <div className={styles.feedContainer}>
       <Navbar />
