8b08e1d6cb3e46ee70acdc8e45b9fbeb0aa7483b
carlosSimplicio <carlos.edsmoura@gmail.com>
2023-02-07 14:37:46 -0300

    mostra umas imagerns aí no feed pra desenvolver

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/feed.module.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
 
 interface FeedProps {
   imagePaths: Array<string>;
@@ -13,7 +14,7 @@ const getFeedImages = async () => {
     const files: Array<string> = await fs.readdir(
       "/home/carlossimplicio/Documents/instagrao/public/feed-images"
     );
-    imagePaths = files.map((file) => "feed-images/" + file);
+    imagePaths = files.map((file) => "/feed-images/" + file);
   } catch (error) {
     console.log(error);
   }
@@ -30,11 +31,27 @@ export const getServerSideProps = async () => {
 };
 
 const Feed: React.FC<FeedProps> = ({ imagePaths }) => {
-  console.log(imagePaths);
+  const i = imagePaths.slice(0, 6);
   return (
     <div className={styles.feedContainer}>
       <Navbar />
-      <div className={styles.feed}></div>
+      <div className={styles.feed}>
+        {i.map((file, index) => {
+          return (
+            <div>
+              <Image
+                key={`${index}-${file}`}
+                src={file}
+                alt="Some image"
+                width={462}
+                height={562}
+                unoptimized
+                priority={index === 0}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
